e60e6fb64095003d8f3de8c21fe34fd5084b5906
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-03 22:52:18 +0530

    030221:2252 getting dummies from X i.e. geography and gender

--- ANN.py
+++ ANN.py
@@ -1,3 +1,4 @@
+# imports !!!!!!!
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -5,4 +6,7 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 
 X = dataset.iloc[:, 3:-1]
 y = dataset.iloc[:, -1]
+# print(X)
+
+X = pd.get_dummies(data = X, columns = ['Geography', 'Gender'])
 # print(X)
