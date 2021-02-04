ed837b5d58de375241c619fcdbb32adb329f4c5c
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-04 23:08:04 +0530

    040221:2308 train test split is done on the dataset

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -10,3 +10,10 @@ dataset = pd.read_csv('Data.csv')
 # #converting categorical data variables to dummy variables
 dataset = pd.get_dummies(data = dataset, columns=['Country', 'Purchased'])
 print(dataset)
+
+
+#splitting independent variables and dependent variables
+X = dataset.iloc[:, :-2].values
+y = dataset.iloc[:,5:7].values
+# print(X)
+
