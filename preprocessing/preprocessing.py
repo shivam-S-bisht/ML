49b0a4d11976e262b91028d0b6c467f41051f5fe
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-04 23:07:05 +0530

    040221:2307 loaded the dataset, getting dummies for the vars

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -3,3 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# importing the dataset
+dataset = pd.read_csv('Data.csv')
+
+
+# #converting categorical data variables to dummy variables
+dataset = pd.get_dummies(data = dataset, columns=['Country', 'Purchased'])
+print(dataset)
