b2d34aff6c5ffc09928608b4eb7594d8fe309116
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-04 23:09:18 +0530

    040221:2309 modifying the dataset by adding the missing data and done fit transform on it

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -17,3 +17,9 @@ X = dataset.iloc[:, :-2].values
 y = dataset.iloc[:,5:7].values
 # print(X)
 
+
+#adding a missing data
+from sklearn.impute import SimpleImputer
+simpleimputer = SimpleImputer(missing_values=np.nan, strategy= 'mean', fill_value=0 )
+X[:, :2] = simpleimputer.fit_transform(X[:, :2])
+print(X)
