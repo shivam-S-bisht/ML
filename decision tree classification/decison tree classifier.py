1252175680002ebdb44c13fb3c9ddfc5d881ce6b
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-06 21:17:19 +0530

    060221:2117 train test split of dependent and independent vars

--- decision tree classification/decison tree classifier.py
+++ decision tree classification/decison tree classifier.py
@@ -5,3 +5,8 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 
 X = dataset.iloc[:, 2:-1]
 y = dataset.iloc[:, -1]
+
+print(X, y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
