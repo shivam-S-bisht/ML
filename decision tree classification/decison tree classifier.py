e398370490783a6df6fcb2620d6a42257a96f87d
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-06 21:18:15 +0530

    060221:2118 decision tree clasifier imported and fits in the classifier

--- decision tree classification/decison tree classifier.py
+++ decision tree classification/decison tree classifier.py
@@ -10,3 +10,8 @@ print(X, y)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'gini', random_state = 0)
+classifier.fit(X_train, y_train)
+
