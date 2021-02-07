0d2406b68252da5f8b9d06312e01597626af9259
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-08 00:05:23 +0530

    080221:0005 decision tree classifier regressor imported

--- decision tree regression/decision tree regression.py
+++ decision tree regression/decision tree regression.py
@@ -8,3 +8,10 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, -1]
 print(y)
+
+#Decision tree classifier from the lib.
+from sklearn.tree import DecisionTreeClassifier
+regressor = DecisionTreeClassifier(random_state = 0)
+regressor.fit(X, y)
+
+
