3b32017db1913e2935af23e2413613250e2c3bc1
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-10 23:26:20 +0530

    100221:2326 clustering classifier imported

--- hierarical clustering/hierarical clustering.py
+++ hierarical clustering/hierarical clustering.py
@@ -5,3 +5,11 @@ dataset = pd.read_csv('Mall_Customers.csv')
 print(dataset)
 
 X = dataset.iloc[:, [3, 4]].values
+print(X)
+
+# import scipy.cluster.hierarchy as sch
+# dendrogram = sch.dendrogram(sch.linkage(X,method = 'ward'))  
+# plt.show()
+
+from sklearn.cluster import AgglomerativeClustering
+classifier = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
