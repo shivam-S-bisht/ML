17923649bdd0ba361052839a568a0f4f08d1f42f
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-10 23:27:51 +0530

    100221:2327 plotting the scatter data of the prediction

--- hierarical clustering/hierarical clustering.py
+++ hierarical clustering/hierarical clustering.py
@@ -15,3 +15,11 @@ from sklearn.cluster import AgglomerativeClustering
 classifier = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
 y_predict = classifier.fit_predict(X)
 
+# plotting the data
+plt.scatter(X[y_predict == 0, 0], X[y_predict == 0, 1], color = 'blue', label = 'careful')
+plt.scatter(X[y_predict == 1, 0], X[y_predict == 1, 1], color = 'black', label = 'Standard')
+plt.scatter(X[y_predict == 2, 0], X[y_predict == 2, 1], color = 'yellow', label = 'Target')
+plt.scatter(X[y_predict == 3, 0], X[y_predict == 3, 1], color = 'red', label = 'Careless')
+plt.scatter(X[y_predict == 4, 0], X[y_predict == 4, 1], color = 'green', label = 'Sensible')
+plt.legend()
+plt.show()
