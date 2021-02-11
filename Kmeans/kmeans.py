6cb9b14d4c9d180e5754db09a4b95585440ec5d9
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-11 17:48:47 +0530

    110221:1748 config for kmean is done

--- Kmeans/kmeans.py
+++ Kmeans/kmeans.py
@@ -7,3 +7,17 @@ X = dataset.iloc[:, [3, 4]].values
 
 from sklearn.cluster import KMeans
 wcss = []
+
+# for i in range(1, 11):
+#     classifier = KMeans(n_clusters = i, init = 'k-means++', random_state = 0, max_iter = 300, n_init = 10)
+#     classifier.fit(X)
+#     wcss.append(classifier.inertia_)
+
+# import matplotlib.pyplot as plt
+# plt.plot(range(1, 11), wcss)
+# plt.title('elbow method')
+# plt.xlabel('no. o fclusters')
+# plt.ylabel('WCSS') 
+# plt.show()  
+
+classifier = KMeans(n_clusters = 5, random_state =0, max_iter = 300, init = 'k-means++', n_init = 10)
