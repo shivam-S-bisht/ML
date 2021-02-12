9c7e3c0e75eddbb77abd028c471f65191638f963
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-12 22:14:44 +0530

    120221:2214 train test split done on 0.25 of the dataset

--- knn/knn.py
+++ knn/knn.py
@@ -6,3 +6,5 @@ y = dataset.iloc[:, -1]
 # print(X, y)
 
 from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
