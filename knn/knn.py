ffbb58da20dbb2547ec205a769c8abb66d1c0989
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-12 22:15:14 +0530

    120221:2215 Knn classifier imported and configged

--- knn/knn.py
+++ knn/knn.py
@@ -8,3 +8,5 @@ y = dataset.iloc[:, -1]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
