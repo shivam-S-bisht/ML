449c5edbe1671932b7e6e8372fc380e786345803
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-12 22:14:18 +0530

    120221:2214 lib imported for train test spilit

--- knn/knn.py
+++ knn/knn.py
@@ -3,3 +3,6 @@ import pandas as pd
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, 2:-1].values
 y = dataset.iloc[:, -1]
+# print(X, y)
+
+from sklearn.model_selection import train_test_split
