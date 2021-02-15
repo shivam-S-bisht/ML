58c2f1d593f8f5bd1ba0b3f93bb6e20f18b5d76b
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-15 22:18:39 +0530

    150221:2218 fit the model in regression model

--- linear regression/linear regression.py
+++ linear regression/linear regression.py
@@ -19,3 +19,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, rand
 #imprting the linear regression
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
