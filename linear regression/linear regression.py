1361dcc3967b517c7161b7a74dee75b85fe3fa86
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-15 22:18:13 +0530

    150221:2218 regression model imported

--- linear regression/linear regression.py
+++ linear regression/linear regression.py
@@ -16,3 +16,6 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
 
 
+#imprting the linear regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
