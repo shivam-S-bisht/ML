2355513718dfaa67409e22544947adbc4dfb615a
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-15 22:16:49 +0530

    150221:2216 dependent and independent vars are saperated

--- linear regression/linear regression.py
+++ linear regression/linear regression.py
@@ -5,3 +5,9 @@ import pandas as pd
 #importng the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 
+
+#independent and dependent variables
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+
