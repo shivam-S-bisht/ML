1a1f1f559d8379484acd4c558585df7e0e54ee4f
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-15 22:17:39 +0530

    150221:2217 train test split from sklearn

--- linear regression/linear regression.py
+++ linear regression/linear regression.py
@@ -11,3 +11,8 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 
+#training and testing 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
+
+
