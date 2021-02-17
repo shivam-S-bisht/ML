bb28748c56d0eb194839749e0c72c254b1cae1c6
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-17 21:53:11 +0530

    170221:2153 train test set spilited from the imported dtaset

--- multiple linear regression/multiple regression.py
+++ multiple linear regression/multiple regression.py
@@ -20,3 +20,9 @@ X = pd.get_dummies(X, columns = ['State'])
 X = X.drop(columns=['State_New York'])
 
 
+
+
+
+#train test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
