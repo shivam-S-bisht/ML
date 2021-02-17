10c6c7f484cb3cf8b2fc45356b72513499ea7ebb
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-17 21:54:12 +0530

    170221:2154 linear regression fit on the train dataset

--- multiple linear regression/multiple regression.py
+++ multiple linear regression/multiple regression.py
@@ -30,3 +30,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, rand
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
+regressor.fit(X_train, y_train)
