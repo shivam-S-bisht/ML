b8608989e8a3d57c37767264fb97d833bb1ee981
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-17 21:54:36 +0530

    170221:2154 predictions to be done on the test set

--- multiple linear regression/multiple regression.py
+++ multiple linear regression/multiple regression.py
@@ -31,3 +31,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, rand
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+#prediction on test set
+y_predict = regressor.predict(X_test)
+
