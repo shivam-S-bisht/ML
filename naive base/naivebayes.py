3c1d1eb036a9aa18affeb9dbe52515c5731a01ba
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-19 21:37:18 +0530

    190221:2137 train and test spilit is done on the dataset

--- naive base/naivebayes.py
+++ naive base/naivebayes.py
@@ -3,3 +3,7 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 
 X = dataset.iloc[:, [3, 4]]
 y = dataset.iloc[:, -1]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
