2404a12383598f998bcfcd862cc093d6835d4875
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-19 21:37:46 +0530

    190221:2137 gaussian classifier of naive base imported and configged

--- naive base/naivebayes.py
+++ naive base/naivebayes.py
@@ -7,3 +7,6 @@ y = dataset.iloc[:, -1]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
