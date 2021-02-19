3b27d6b5a7bb1f05decd509832552b86619c8559
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-19 21:38:50 +0530

    190221:2138 predictions are calculated on the test set

--- naive base/naivebayes.py
+++ naive base/naivebayes.py
@@ -11,3 +11,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
+ 
+#predictions
+y_predict = classifier.predict(X_test)
