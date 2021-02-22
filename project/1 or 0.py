481677fdf79f6c971cc9956198af99a2a3ca6f51
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-22 23:32:27 +0530

    220221:2332 append ones to the independent vars

--- project/1 or 0.py
+++ project/1 or 0.py
@@ -9,3 +9,14 @@ y = dataset.iloc[:, -1]
 
 X = pd.get_dummies(data = X, columns = ['month'])
 X = X.drop(columns = ['month_sep'])
+
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# from sklearn.linear_model import LinearRegression
+# regressor = LinearRegression()
+# regressor.fit(X_train, y_train)
+# y_predict = regressor.predict(X_test)
+
+X = np.append(arr = np.ones((515, 1)).astype(int), values = X, axis = 1)
+X = pd.DataFrame(X)
