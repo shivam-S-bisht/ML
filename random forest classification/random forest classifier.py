892fa6a899155ac7f8ab1ffde723a6a3fc4e34df
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-23 22:46:29 +0530

    230221:2246 train test spilit done on the dataset

--- random forest classification/random forest classifier.py
+++ random forest classification/random forest classifier.py
@@ -11,3 +11,6 @@ from sklearn.preprocessing import StandardScaler
 ss_X = StandardScaler()
 X = ss_X.fit_transform(X)
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
