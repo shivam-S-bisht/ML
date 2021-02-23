74cd12aabd29f64841e0ea14525c81502a00e0a5
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-23 22:45:55 +0530

    230221:2245 scaling done on independent vars

--- random forest classification/random forest classifier.py
+++ random forest classification/random forest classifier.py
@@ -5,3 +5,9 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, 2:-1]
 y = dataset.iloc[:, -1]
 
+
+
+from sklearn.preprocessing import StandardScaler
+ss_X = StandardScaler()
+X = ss_X.fit_transform(X)
+
