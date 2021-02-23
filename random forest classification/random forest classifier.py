2033309675a3b3118022cdaeee036a6c735d7d68
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-23 22:47:33 +0530

    230221:2247 random forest classfier imported and configged

--- random forest classification/random forest classifier.py
+++ random forest classification/random forest classifier.py
@@ -14,3 +14,5 @@ y = dataset.iloc[:, -1]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(criterion = 'entropy', random_state = 0, n_estimators = 10, max_depth=10, max_leaf_nodes=20)
