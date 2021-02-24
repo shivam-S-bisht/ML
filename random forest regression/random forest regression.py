90b46b66206b84d40cecf7950471d66335a51d70
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-24 23:15:28 +0530

    240221:2315 form ensemble random forest regressor is imported and configged

--- random forest regression/random forest regression.py
+++ random forest regression/random forest regression.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2]
 y = dataset.iloc[:, -1]
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
