1d207942f4102627f7372c4ffc65307231c004cc
Shivam Bisht <bishtshivam096@gmail.com>
2021-02-25 23:43:27 +0530

    250221:2343 support vector classifier imported and configged

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -11,3 +11,7 @@ y = dataset.iloc[:, -1]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+
+
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
